afb1b626fb5a5878f5841ddf17b85e2feb01f0d9
productmgrdiva <sarika_kanabar@yahoo.com>
2013-07-25 00:17:50 +0000

    images added

--- web.js
+++ web.js
@@ -7,6 +7,8 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
 });
 
+app.use("/images", express.static(__dirname + "/images"));
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
